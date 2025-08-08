d4c8ba2a2f09b0ce1f640c5696b999cfb4d32fea
nlankwei5 <lampteyebenezer220@gmail.com>
2025-08-08 13:39:22 +0000

    Migration bug fix

--- api/models.py
+++ api/models.py
@@ -37,8 +37,7 @@ class CustomUser (AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=150, unique=True)
     first_name = models.CharField(max_length=150, blank=True)
     last_name = models.CharField(max_length=150, blank=True)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False) 
+    is_active = models.BooleanField(default=True) 
     is_superuser = models.BooleanField(default=False)
     is_uploader = models.BooleanField(default=False) 
 
@@ -68,6 +67,8 @@ class MusicSheet(models.Model):
 
     def __str__(self):
         return self.title
+
+
  
 
 
