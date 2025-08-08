e24df89aa41b9c860f7cb83ec472f80ccacce6f8
nlankwei5 <lampteyebenezer220@gmail.com>
2025-08-08 11:36:53 +0000

    update urls and create permissions.py

---
+++ api/permissions.py
@@ -0,0 +1,6 @@
+from rest_framework import permissions
+
+
+class isUploaderOrReadOnly(permissions.BasePermission):
+    
+    pass
---
+++ api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path 
+from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+url_patterns=[
+
+
+
+] +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
