c2375bf43c7e7a8d58ea78feb5862dd885b88c6b
nlankwei5 <lampteyebenezer220@gmail.com>
2025-08-12 10:24:41 +0000

    Update url.py with djoser endpoints

--- api/urls.py
+++ api/urls.py
@@ -5,8 +5,9 @@ from django.conf.urls.static import static
 
 
 url_patterns=[
-
-
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')), 
 
 ] +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
