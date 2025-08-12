4de65e98fddb00c2d9ecafc1a07a92087f922310
nlankwei5 <lampteyebenezer220@gmail.com>
2025-08-12 14:17:56 +0000

    Update settings
    Added Djoser settings to setting.py

--- dycMusiclib/settings.py
+++ dycMusiclib/settings.py
@@ -142,3 +142,11 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAdminUser',
     ],
 }
+
+DJOSER = {
+    'PASSWORD_RESET_CONFIRM_URL': '#/password/reset/confirm/{uid}/{token}',
+    'USERNAME_RESET_CONFIRM_URL': '#/username/reset/confirm/{uid}/{token}',
+    'ACTIVATION_URL': '#/activate/{uid}/{token}',
+    'SEND_ACTIVATION_EMAIL': True,
+    'SERIALIZERS': {},
+}
