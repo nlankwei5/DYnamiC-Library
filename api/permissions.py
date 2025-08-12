2b2a154ca29cc5ec0aa9e4eecd977f18142ff91e
nlankwei5 <lampteyebenezer220@gmail.com>
2025-08-12 09:45:56 +0000

    Created permissions for users who are only assigned uploading feature

--- api/permissions.py
+++ api/permissions.py
@@ -3,4 +3,8 @@ from rest_framework import permissions
 
 class isUploaderOrReadOnly(permissions.BasePermission):
     
-    pass
+    def has_uploader_permissions (self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True 
+
+        return obj.uploader == request.user     
