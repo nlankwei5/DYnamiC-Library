7aaa85bec6ba33f77d517c413079019791efc408
nlankwei5 <lampteyebenezer220@gmail.com>
2025-08-12 09:49:59 +0000

    Updated permissions

--- api/permissions.py
+++ api/permissions.py
@@ -3,8 +3,8 @@ from rest_framework import permissions
 
 class isUploaderOrReadOnly(permissions.BasePermission):
     
-    def has_uploader_permissions (self, request, view, obj):
+    def has_object_permissions (self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True 
 
-        return obj.uploader == request.user     
+        return obj.uploaded_by == request.user     
