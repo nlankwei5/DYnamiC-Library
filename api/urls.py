177e807f312d58314dfe07f979c42fb9c35cacda
nlankwei5 <lampteyebenezer220@gmail.com>
2025-08-13 10:46:54 +0000

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-urlpatterns=[
+urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')), 
 
