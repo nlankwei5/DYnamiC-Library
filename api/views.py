8a35b4e91b2a80230f27c47bc57d49ed07cdba89
nlankwei5 <lampteyebenezer220@gmail.com>
2025-08-18 11:42:12 +0000

    Create views.py

--- api/views.py
+++ api/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .serializer import * 
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from .models import *
+from rest_framework import viewsets
+
+
 
 # Create your views here.
+
+class MusicSheetViewSet(viewsets.ModelViewSet):
+    queryset = MusicSheet.objects.all()
+    serializer_class = MusicSheetSerializer
+    
+    def get_permissions(self):
+
+        if self.action == ['create','update', 'partial_update', 'destroy']:
+            permission_classes = [IsAdminUser]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+    
+    def perform_create(self, serializer):
+        serializer.save(uploaded_by = self.request.user)
+
+    
+    
+
