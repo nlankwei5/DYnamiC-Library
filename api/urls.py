852ae261b4cb3fa76390cddfc7b11a2d8fd01987
nlankwei5 <lampteyebenezer220@gmail.com>
2025-08-18 12:29:16 +0000

    Add view routers to urls.py

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from .views import *
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.routers import DefaultRouter
 
 
 urlpatterns = [
@@ -11,3 +12,6 @@ urlpatterns = [
 ] +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
+router = DefaultRouter()
+router.register(r'music-sheet', MusicSheetViewSet, basename='music-sheet')
+urlpatterns += router.urls
